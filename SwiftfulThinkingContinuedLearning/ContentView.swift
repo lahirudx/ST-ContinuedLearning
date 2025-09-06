6d8070a15a4065a7a7ef79725b0c1a2ad0bafaa1
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-09-06 20:01:30 +0530

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Lahiru Amarasooriya on 2025-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Lahiru Amarasooriya on 2025-09-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
