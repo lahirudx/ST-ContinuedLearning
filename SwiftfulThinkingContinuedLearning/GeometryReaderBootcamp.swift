d290a4a82008de3708a0e1b89b2b853a8b7a321a
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-09-07 14:18:21 +0530

    6. GeometryReader in SwiftUI to get a view's size and location

--- SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct GeometryReaderBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.blue)
+                            .rotation3DEffect(Angle(degrees: getPercentage(of: geometry) * 40), axis: (x: 0, y: 1, z: 0))
+                           
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                        
+                }
+            }
+        }
+        
+//        GeometryReader { geometry in
+//            HStack(spacing: 0.0) {
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: geometry.size.width * 0.6666)
+//                
+//                Rectangle()
+//                    .fill(.blue)
+//            }
+//            .ignoresSafeArea()
+//        }
+    }
+    
+    func getPercentage(of geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let distance = geo.frame(in: .global).midX
+        return Double(1 - (distance / maxDistance))
     }
 }
 
