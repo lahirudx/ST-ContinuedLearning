852ffebfacc58c4064ce507b6eed7ece82d3c66e
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-09-07 14:18:03 +0530

    6. GeometryReader in SwiftUI to get a view's size and location

---
+++ SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Lahiru Amarasooriya on 2025-09-07.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReaderBootcamp()
+}
