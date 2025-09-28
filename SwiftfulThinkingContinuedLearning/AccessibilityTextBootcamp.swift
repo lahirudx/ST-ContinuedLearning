64fa9a7ad7f8a33aceb1462e08e48f39f2a0b94e
Lahiru Amarasooriya <lahirudx@gmail.com>
2025-09-28 09:55:11 +0530

    29. Accessibility in Swift: Dynamic Text

---
+++ SwiftfulThinkingContinuedLearning/AccessibilityTextBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityTextBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Lahiru Amarasooriya on 2025-09-28.
+//
+
+import SwiftUI
+
+struct AccessibilityTextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityTextBootcamp()
+}
